709e07cdfa93fa2e49c4e124fe5a848c7685eb66
Muhammad Usman <usmanisofficial@outlook.com>
2021-08-05 01:13:58 +0500

    completdeUI

--- lib/pages/calculator.dart
+++ lib/pages/calculator.dart
@@ -2,28 +2,57 @@ import 'package:calculator/widgets/theme,.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
+// const List firstRow = [];
+// const List secondRow = [];
+// const List thirRow = [];
+// const List fourthRow = [];
+const Map labels = {
+  "firstRow": ["AC", "+/-", "%", "÷"],
+  "secondRow": ["7", "8", "9", "X"],
+  "thirdRow": ["4", "5", "6", "-"],
+  "fourthRow": ["1", "2", "3", "+"],
+  "fifthRow": ["0", ".", "="],
+};
+
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      //  backgroundColor(Colors.accents),
-
       child: Container(
-        color: Colors.red,
         child: Scaffold(
             body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Container(
-            padding: EdgeInsets.only(top: 150),
+            padding: EdgeInsets.only(top: 180),
             child: ListView(
-              // padding: Vx.mH8,
               children: [
-                "0".text.xl6.align(TextAlign.end).make(),
-                MyButtons(),
-
-                // ElevatedButton(onPressed: null, child: "AC".text.make())
+                Screen(),
+                MyButtons(
+                  labels: labels["firstRow"],
+                  c1: MyThemes.greyColor,
+                  c2: MyThemes.yellowColor,
+                ).pOnly(bottom: 20),
+                MyButtons(
+                  labels: labels["secondRow"],
+                  c1: MyThemes.darkGreyColor,
+                  c2: MyThemes.yellowColor,
+                ).pOnly(bottom: 20),
+                MyButtons(
+                  labels: labels["thirdRow"],
+                  c1: MyThemes.darkGreyColor,
+                  c2: MyThemes.yellowColor,
+                ).pOnly(bottom: 20),
+                MyButtons(
+                  labels: labels["fourthRow"],
+                  c1: MyThemes.darkGreyColor,
+                  c2: MyThemes.yellowColor,
+                ).pOnly(bottom: 20),
+                MyButtonsL(
+                    labels: labels["fifthRow"],
+                    c1: MyThemes.darkGreyColor,
+                    c2: MyThemes.yellowColor)
               ],
             ),
           ),
@@ -34,7 +63,34 @@ class Home extends StatelessWidget {
 }
 
 class MyButtons extends StatelessWidget {
-  const MyButtons({Key? key}) : super(key: key);
+  final List labels;
+  final Color c1;
+  final Color c2;
+  const MyButtons(
+      {Key? key, required this.labels, required this.c1, required this.c2})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        MyB(label: labels[0], color: c1).pOnly(left: 10),
+        MyB(label: labels[1], color: c1).pOnly(left: 10),
+        MyB(label: labels[2], color: c1).pOnly(left: 10),
+        MyB(label: labels[3], color: c2).pOnly(left: 10, right: 10),
+      ],
+    );
+  }
+}
+
+class MyButtonsL extends StatelessWidget {
+  final List labels;
+  final Color c1;
+  final Color c2;
+  const MyButtonsL(
+      {Key? key, required this.labels, required this.c1, required this.c2})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -43,38 +99,49 @@ class MyButtons extends StatelessWidget {
       children: [
         ElevatedButton(
           onPressed: () {},
-          child: "AC".text.make(),
+          child: Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 20),
+              child:
+                  "0".text.start.align(TextAlign.start).minFontSize(30).make()),
           style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(MyThemes.creamColor),
-            shape: MaterialStateProperty.all(CircleBorder()),
-            // fixedSize: MaterialStateProperty.all(Size(80, 80)),
+            backgroundColor: MaterialStateProperty.all(MyThemes.darkGreyColor),
+            shape: MaterialStateProperty.all(StadiumBorder()),
+            fixedSize: MaterialStateProperty.all(Size(170, 80)),
+            // textStyle: MaterialStateProperty.all(),
           ),
-        ).pOnly(right: 10),
-        ElevatedButton(
-          onPressed: () {},
-          child: "+/-".text.make(),
-          style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(MyThemes.creamColor),
-              shape: MaterialStateProperty.all(CircleBorder()),
-              fixedSize: MaterialStateProperty.all(Size(80, 80))),
-        ).pOnly(right: 10),
-        ElevatedButton(
-          onPressed: () {},
-          child: "AC".text.make(),
-          style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(MyThemes.creamColor),
-              shape: MaterialStateProperty.all(CircleBorder()),
-              fixedSize: MaterialStateProperty.all(Size(80, 80))),
-        ).pOnly(right: 10),
-        ElevatedButton(
-          onPressed: () {},
-          child: "AC".text.make(),
-          style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(MyThemes.creamColor),
-              shape: MaterialStateProperty.all(CircleBorder()),
-              fixedSize: MaterialStateProperty.all(Size(80, 80))),
-        ),
+        ).pOnly(left: 10),
+        MyB(label: labels[1], color: c1).pOnly(left: 10),
+        MyB(label: labels[2], color: c2).pOnly(left: 10, right: 10),
       ],
     );
+  }
+}
+
+class MyB extends StatelessWidget {
+  final String label;
+  final Color color;
+  const MyB({Key? key, required this.label, required this.color})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: label.text.align(TextAlign.center).minFontSize(30).make(),
+      style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(color),
+          shape: MaterialStateProperty.all(CircleBorder()),
+          fixedSize: MaterialStateProperty.all(Size(80, 80))),
+    );
+  }
+}
+
+class Screen extends StatelessWidget {
+  const Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return "0".text.xl6.align(TextAlign.end).make().pOnly(right: 30);
   }
 }
--- lib/widgets/theme,.dart
+++ lib/widgets/theme,.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class MyThemes {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
       primarySwatch: Colors.grey,
-      fontFamily: GoogleFonts.poppins().fontFamily,
+      fontFamily: GoogleFonts.arizonia().fontFamily,
       backgroundColor: Colors.black,
       appBarTheme: AppBarTheme(
           color: Colors.black,
@@ -20,6 +20,7 @@ class MyThemes {
       );
   static ThemeData blackTheme(BuildContext context) => ThemeData(
         scaffoldBackgroundColor: Colors.black,
+        fontFamily: GoogleFonts.poppins().fontFamily,
         textTheme: TextTheme(
                 bodyText1: TextStyle(),
                 bodyText2: TextStyle(),
@@ -31,6 +32,8 @@ class MyThemes {
       );
 
   //Colors
-  static Color creamColor = Color.fromARGB(165, 165, 165, 165);
+  static Color greyColor = Color(0xffa5a5a5);
+  static Color darkGreyColor = Color(0xff333333);
+  static Color yellowColor = Color(0xffff9e0a);
   static Color darkBluishColor = Color(0xff403b58);
 }
